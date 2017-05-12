a51146b7e112c9742554d78d344e874f212ac29c
gordominossi <gabriel.minossi@gmail.com>
2017-05-12 11:06:32 -0300

    Corrigida a formatação.

--- src/trainer/Item.java
+++ src/trainer/Item.java
@@ -3,6 +3,7 @@ package trainer;
 public class Item {
 	private String name;
 	private int healPower;
+
 	public Item(String itemName, int healPower) {
 		this.name = itemName;
 		this.healPower = healPower;
--- src/trainer/Trainer.java
+++ src/trainer/Trainer.java
@@ -5,31 +5,31 @@ import java.util.ArrayList;
 import pokemon.Pokemon;
 
 public class Trainer {
-	
+
 	public static final int MAX_POKEMONS = 6;
 	private String name;
 	private Pokemon pokemons[] = new Pokemon[MAX_POKEMONS];
 	private int numberOfPokemons = 0;
 	private ArrayList<Item> items;
-	
+
 	public Trainer(String trainerName, Pokemon pokemons[], ArrayList<Item> items) {
 		this.name = trainerName;
 		this.pokemons = pokemons;
 		this.items = items;
 	}
-	
+
 	public boolean addPokemon(Pokemon pokemon) {
 		if (numberOfPokemons < MAX_POKEMONS) {
 			pokemons[numberOfPokemons++] = pokemon;
 			return true;
-		}
-		else
-			return false;			
+		} else
+			return false;
 	}
-	// Deletes the selected Pokémon if possible and shifts the vacant space to the end of the array.
+
+	// Deletes the selected Pokémon if possible and shifts the vacant space to
+	// the end of the array.
 	public boolean removePokemon(int pokemonIndex) {
-		if (pokemonIndex < 0 || pokemonIndex > numberOfPokemons || 
-				pokemons[pokemonIndex] == null) {
+		if (pokemonIndex < 0 || pokemonIndex > numberOfPokemons || pokemons[pokemonIndex] == null) {
 			return false;
 		} else {
 			for (int i = pokemonIndex; i < numberOfPokemons - 1; i++) {
@@ -39,15 +39,19 @@ public class Trainer {
 			return true;
 		}
 	}
+
 	public int getNumberOfPokemons() {
 		return numberOfPokemons;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public Pokemon[] getPokemons() {
 		return pokemons;
 	}
+
 	public ArrayList<Item> getItems() {
 		return items;
 	}
